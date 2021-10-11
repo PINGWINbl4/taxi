3caead8c14f0c695675af0c3a03700ef65436309
Ping2win <40270221+PINGWINbl4@users.noreply.github.com>
2021-10-11 12:37:35 +0500

    Добавлены комментарии
    Обработка переменных и диалог с пользователем были добавлены ранее

--- main_taxi.py
+++ main_taxi.py
@@ -1,4 +1,4 @@
-while True:
+while True:     # Ввод колличества сотрудников и обработка на ошибки ввода
     try:
         numb = int(input('Введите колличество сотрудников'))
         break
@@ -10,7 +10,7 @@ taxi = []
 final_list = []
 print('Введите расстояния до домов сотрудников')
 
-for i in range(numb):
+for i in range(numb):   # Ввод расстояния сотрудников, обработка на ошибки ввода и внесение в массив
     while True:
         try:
             Value = int(input('Расстояние до дома сотрудника'))
@@ -21,7 +21,7 @@ for i in range(numb):
     final_list.append(Value)
 print('Введите тарифы таксистов')
 
-for i in range(numb):
+for i in range(numb):   # Ввод тарифа таксиста, обработка на ошибки ввода и внесение в массив
     while True:
         try:
             Value = int(input('Тариф таксиста'))
@@ -30,7 +30,7 @@ for i in range(numb):
             print(print('Некорректное значение'))
     taxi.append(Value)
 
-for i in range(len(sotr)):
+for i in range(len(sotr)):  # Анализ данных, сортировка иформирование результирующего массива
     final_list[sotr.index(min(sotr))] = taxi.index(max(taxi))+1
     sotr[sotr.index(min(sotr))] = 1001
     taxi[taxi.index(max(taxi))] = -1
